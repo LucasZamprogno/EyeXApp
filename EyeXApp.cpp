458a5cace00f168261411118e3c51cf6008ae0fb
Lucas Zamprogno <lucas.zamprogno@gmail.com>
2017-05-23 23:19:56 -0700

    File upload

---
+++ EyeXApp.cpp
@@ -0,0 +1,192 @@
+/*
+* Slightly modified version of the Tobii SampleEyeXApp
+*/
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <conio.h>
+#include <assert.h>
+#include <eyex/EyeX.h>
+#include <curl/curl.h>
+
+#pragma comment (lib, "Tobii.EyeX.Client.lib")
+
+// ID of the global interactor that provides our data stream; must be unique within the application.
+static const TX_STRING InteractorId = "Interaction Monitor";
+
+// global variables
+static TX_HANDLE g_hGlobalInteractorSnapshot = TX_EMPTY_HANDLE;
+
+CURL *curl;
+CURLcode res;
+
+/*
+* Initializes g_hGlobalInteractorSnapshot with an interactor that has the Gaze Point behavior.
+*/
+BOOL InitializeGlobalInteractorSnapshot(TX_CONTEXTHANDLE hContext)
+{
+	TX_HANDLE hInteractor = TX_EMPTY_HANDLE;
+	TX_GAZEPOINTDATAPARAMS params = { TX_GAZEPOINTDATAMODE_LIGHTLYFILTERED };
+	BOOL success;
+
+	success = txCreateGlobalInteractorSnapshot(
+		hContext,
+		InteractorId,
+		&g_hGlobalInteractorSnapshot,
+		&hInteractor) == TX_RESULT_OK;
+	success &= txCreateGazePointDataBehavior(hInteractor, &params) == TX_RESULT_OK;
+
+	txReleaseObject(&hInteractor);
+
+	return success;
+}
+
+void libcurlPostToServer(int x, int y) {
+	std::string postData = "x=" + std::to_string(x) + "&y=" + std::to_string(y);
+	const char * c = postData.c_str();
+	//std::string postData;
+	//std::stringstream s;
+	//s << "x=" << x << "&y=" << y;
+	//postData = s.str();
+	printf("Gaze Data: (%d, %d)\n", x, y);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, c);
+	res = curl_easy_perform(curl);
+	if (res != CURLE_OK)
+		fprintf(stderr, "curl_easy_perform() failed: %s\n",
+			curl_easy_strerror(res));
+}
+
+/*
+* Callback function invoked when a snapshot has been committed.
+*/
+void TX_CALLCONVENTION OnSnapshotCommitted(TX_CONSTHANDLE hAsyncData, TX_USERPARAM param)
+{
+	// check the result code using an assertion.
+	// this will catch validation errors and runtime errors in debug builds. in release builds it won't do anything.
+
+	TX_RESULT result = TX_RESULT_UNKNOWN;
+	txGetAsyncDataResultCode(hAsyncData, &result);
+	assert(result == TX_RESULT_OK || result == TX_RESULT_CANCELLED);
+}
+
+/*
+* Callback function invoked when the status of the connection to the EyeX Engine has changed.
+*/
+void TX_CALLCONVENTION OnEngineConnectionStateChanged(TX_CONNECTIONSTATE connectionState, TX_USERPARAM userParam)
+{
+	switch (connectionState) {
+	case TX_CONNECTIONSTATE_CONNECTED: {
+		BOOL success;
+		printf("The connection state is now CONNECTED (We are connected to the EyeX Engine)\n");
+		// commit the snapshot with the global interactor as soon as the connection to the engine is established.
+		// (it cannot be done earlier because committing means "send to the engine".)
+		success = txCommitSnapshotAsync(g_hGlobalInteractorSnapshot, OnSnapshotCommitted, NULL) == TX_RESULT_OK;
+		if (!success) {
+			printf("Failed to initialize the data stream.\n");
+		}
+		else {
+			printf("Waiting for gaze data to start streaming...\n");
+		}
+	}
+									   break;
+
+	case TX_CONNECTIONSTATE_DISCONNECTED:
+		printf("The connection state is now DISCONNECTED (We are disconnected from the EyeX Engine)\n");
+		break;
+
+	case TX_CONNECTIONSTATE_TRYINGTOCONNECT:
+		printf("The connection state is now TRYINGTOCONNECT (We are trying to connect to the EyeX Engine)\n");
+		break;
+
+	case TX_CONNECTIONSTATE_SERVERVERSIONTOOLOW:
+		printf("The connection state is now SERVER_VERSION_TOO_LOW: this application requires a more recent version of the EyeX Engine to run.\n");
+		break;
+
+	case TX_CONNECTIONSTATE_SERVERVERSIONTOOHIGH:
+		printf("The connection state is now SERVER_VERSION_TOO_HIGH: this application requires an older version of the EyeX Engine to run.\n");
+		break;
+	}
+}
+
+/*
+* Handles an event from the Gaze Point data stream.
+*/
+void OnGazeDataEvent(TX_HANDLE hGazeDataBehavior)
+{
+	TX_GAZEPOINTDATAEVENTPARAMS eventParams;
+	if (txGetGazePointDataEventParams(hGazeDataBehavior, &eventParams) == TX_RESULT_OK) {
+		libcurlPostToServer(eventParams.X, eventParams.Y); // Causes warning, TX_REAL to int. Works for our purposes
+	}
+	else {
+		printf("Failed to interpret gaze data event packet.\n");
+	}
+}
+
+/*
+* Callback function invoked when an event has been received from the EyeX Engine.
+*/
+void TX_CALLCONVENTION HandleEvent(TX_CONSTHANDLE hAsyncData, TX_USERPARAM userParam)
+{
+	TX_HANDLE hEvent = TX_EMPTY_HANDLE;
+	TX_HANDLE hBehavior = TX_EMPTY_HANDLE;
+
+	txGetAsyncDataContent(hAsyncData, &hEvent);
+
+	if (txGetEventBehavior(hEvent, &hBehavior, TX_BEHAVIORTYPE_GAZEPOINTDATA) == TX_RESULT_OK) {
+		OnGazeDataEvent(hBehavior);
+		txReleaseObject(&hBehavior);
+	}
+
+	txReleaseObject(&hEvent);
+}
+
+int main(int argc, char* argv[])
+{
+	curl_global_init(CURL_GLOBAL_ALL);
+	curl = curl_easy_init();
+	// Disable https verifications
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+	curl_easy_setopt(curl, CURLOPT_URL, "https://localhost:4321/coordinate");
+
+	TX_CONTEXTHANDLE hContext = TX_EMPTY_HANDLE;
+	TX_TICKET hConnectionStateChangedTicket = TX_INVALID_TICKET;
+	TX_TICKET hEventHandlerTicket = TX_INVALID_TICKET;
+	BOOL success;
+
+	// initialize and enable the context that is our link to the EyeX Engine.
+	success = txInitializeEyeX(TX_EYEXCOMPONENTOVERRIDEFLAG_NONE, NULL, NULL, NULL, NULL) == TX_RESULT_OK;
+	success &= txCreateContext(&hContext, TX_FALSE) == TX_RESULT_OK;
+	success &= InitializeGlobalInteractorSnapshot(hContext);
+	success &= txRegisterConnectionStateChangedHandler(hContext, &hConnectionStateChangedTicket, OnEngineConnectionStateChanged, NULL) == TX_RESULT_OK;
+	success &= txRegisterEventHandler(hContext, &hEventHandlerTicket, HandleEvent, NULL) == TX_RESULT_OK;
+	success &= txEnableConnection(hContext) == TX_RESULT_OK;
+
+	// Let the events flow until a key is pressed.
+	// May want to make this a more specific exit condition
+	if (success) {
+		printf("Initialization was successful.\n");
+	}
+	else {
+		printf("Initialization failed.\n");
+	}
+	printf("Press any key to exit...\n");
+	_getch();
+	printf("Exiting.\n");
+
+	// disable and delete the context.
+	txDisableConnection(hContext);
+	txReleaseObject(&g_hGlobalInteractorSnapshot);
+	success = txShutdownContext(hContext, TX_CLEANUPTIMEOUT_DEFAULT, TX_FALSE) == TX_RESULT_OK;
+	success &= txReleaseContext(&hContext) == TX_RESULT_OK;
+	success &= txUninitializeEyeX() == TX_RESULT_OK;
+	if (!success) {
+		printf("EyeX could not be shut down cleanly. Did you remember to release all handles?\n");
+	}
+	curl_easy_cleanup(curl);
+	curl_global_cleanup();
+	return 0;
+}
